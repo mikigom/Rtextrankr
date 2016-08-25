903f20943df37d01e1c459a5ce8f7fdbc2c82d28
mikigom <sjh14@gist.ac.kr>
2016-08-25 15:30:59 +0900

    CRAN submit ready

--- R/Rtextrankr.R
+++ R/Rtextrankr.R
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Some useful keyboard shortcuts for package authoring: Build and
-# Reload Package: 'Ctrl + Shift + B' Check Package: 'Ctrl + Shift + E'
-# Test Package: 'Ctrl + Shift + T'
-
 #' @export xplit
 #' @export sentence2table
 #' @export table2gset
